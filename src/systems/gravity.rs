587b35b9e79024d685c59d1f5b48284624b5b94c
Sam Szuflita <sam.szuflita@gmail.com>
2020-04-22 23:43:07 -0400

    Remove warning

--- src/systems/gravity.rs
+++ src/systems/gravity.rs
@@ -11,7 +11,7 @@ impl<'s> System<'s> for GravitySystem {
     type SystemData = (ReadStorage<'s, Thing>, WriteStorage<'s, Transform>);
 
     fn run(&mut self, (things, mut locals): Self::SystemData) {
-        for (thing, local) in (&things, &mut locals).join() {
+        for (_thing, local) in (&things, &mut locals).join() {
             local.prepend_translation_y(-1.);
         }
     }
