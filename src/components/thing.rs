33affd445162fc26bce134c6f4916442092c4f14
Sam Szuflita <sam.szuflita@gmail.com>
2020-04-22 23:41:05 -0400

    Basic implementation of falling thing

---
+++ src/components/thing.rs
@@ -0,0 +1,17 @@
+use amethyst::ecs::prelude::{Component, DenseVecStorage};
+
+
+pub struct Thing {}
+
+impl Component for Thing {
+    type Storage = DenseVecStorage<Self>;
+
+}
+
+impl Thing {
+    pub fn new(
+    ) -> Thing {
+        Thing {
+        }
+    }
+}
