c5305f9973e3d1dd2fe41776866cfb01c3c7bcc7
Sam Szuflita <sam@thisisalice.com>
2020-04-28 17:15:38 -0400

    Remove gravity system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,12 +1,10 @@
 mod atop;
 mod corner;
 mod escalator;
-mod gravity;
 mod movement;
 mod utils;
 
 pub use self::atop::AtopSystem;
 pub use self::corner::CornerSystem;
 pub use self::escalator::EscalatorSystem;
-pub use self::gravity::GravitySystem;
 pub use self::movement::MoveSystem;
