90ec60062abdd97f42100c01585a254f5b81ab65
Sam Szuflita <sam.szuflita@gmail.com>
2020-05-05 08:57:14 -0400

    Remove unnecessary register

--- src/entities/rewindable_clock.rs
+++ src/entities/rewindable_clock.rs
@@ -2,6 +2,5 @@ use crate::components::RewindableClock;
 use amethyst::prelude::*;
 
 pub fn initialize_clock(world: &mut World) {
-    world.register::<RewindableClock>();
     world.create_entity().with(RewindableClock::new()).build();
 }
