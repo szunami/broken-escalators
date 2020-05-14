1dfee36f893410a0ea1f8e2364501958b1f579cd
Sam Szuflita <sam.szuflita@gmail.com>
2020-05-14 09:39:20 -0400

    Float formatting

--- src/utils/color.rs
+++ src/utils/color.rs
@@ -12,7 +12,7 @@ impl ColorFlag {
     pub fn to_srgba(self) -> Srgba {
         match self {
             ColorFlag::WHITE => Srgba::new(1.0, 1.0, 1.0, 1.0),
-            ColorFlag::YELLOW => Srgba::new(1.0, 0.98046875, 0., 1.0),
+            ColorFlag::YELLOW => Srgba::new(1.0, 0.980_468_75, 0., 1.0),
             ColorFlag::BLUE => Srgba::new(0.30,0.65,1.00, 1.0),
         }
     }
