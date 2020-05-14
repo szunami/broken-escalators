c41d52d61204d94f047ab6a0c32f23559817703e
Sam Szuflita <sam.szuflita@gmail.com>
2020-05-14 13:22:39 -0400

    Clean up test

--- tests/level_test.rs
+++ tests/level_test.rs
@@ -7,11 +7,8 @@ use std::fs;
 fn all_levels_deser() {
     let level_paths = fs::read_dir("assets/levels").unwrap();
     for path in level_paths {
-        let x = path.unwrap();
-        let y = x.path();
-        let z = y.as_path();
-        println!("{}", z.display());
-        let load_result: Result<_, _> = LevelConfig::load(z);
+        let actual_path = path.unwrap().path();
+        let load_result: Result<_, _> = LevelConfig::load(actual_path.as_path());
         println!("{:?}", load_result);
         assert!(!load_result.is_err());
     }
