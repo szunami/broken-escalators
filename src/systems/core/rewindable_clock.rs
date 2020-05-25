8ea7e1f362a467cc995e4403e1d6cc59c4b168c4
Sam Szuflita <sam.szuflita@gmail.com>
2020-05-25 17:39:59 -0400

    Use lr not z space

--- src/systems/core/rewindable_clock.rs
+++ src/systems/core/rewindable_clock.rs
@@ -12,9 +12,9 @@ impl<'s> System<'s> for RewindableClockSystem {
     type SystemData = (Read<'s, DownKeys>, Write<'s, RewindableClock>);
 
     fn run(&mut self, (down_keys, mut clock): Self::SystemData) {
-        let velocity = if down_keys.key_downs().contains(&VirtualKeyCode::Space) {
+        let velocity = if down_keys.key_downs().contains(&VirtualKeyCode::Right) {
             1
-        } else if down_keys.key_downs().contains(&VirtualKeyCode::Z) {
+        } else if down_keys.key_downs().contains(&VirtualKeyCode::Left) {
             -1
         } else {
             0
